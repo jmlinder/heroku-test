cb76d9c60aa15758dd3b1283f8c21033e648e7d8
Joe <joemlinder@gmail.com>
2019-03-24 12:50:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(3000, console.log('server has started'));
